d8fc80affff7ab966818af40aea9ae86674c5845
Jeremy Coyle <jeremyrcoyle@gmail.com>
2018-04-30 10:51:53 -0700

    plot_improvements

--- R/plot_results.R
+++ R/plot_results.R
@@ -7,7 +7,8 @@ tsm_plot <- function(formatted_results){
   #todo: generalize faceting
   ggplot(tsms,aes_string(x="intervention_level", y="estimate", ymin="ci_lower", ymax="ci_upper"))+
     geom_point()+geom_errorbar()+facet_wrap(~strata_label)+
-    xlab(intervention)+ylab("Estimate")+theme_bw()+ggtitle("Treatment Specific Means")
+    xlab(intervention)+ylab("Estimate")+theme_bw()+ggtitle("Treatment Specific Means")+
+    theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
 }
 
 #' @import ggplot2
@@ -16,11 +17,12 @@ rr_plot <- function(formatted_results){
   rrs <- formatted_results[type=="RR"]
   intervention <- rrs$intervention_variable[1]
   outcome <- rrs$outcome_variable[1]
-  baseline <- rrs$baseline_line[1]
+  baseline <- rrs$baseline_level[1]
   #todo: generalize faceting
   ggplot(rrs,aes_string(x="intervention_level", y="estimate", ymin="ci_lower", ymax="ci_upper"))+
     geom_point()+geom_errorbar()+facet_wrap(~strata_label)+
-    xlab(intervention)+ylab("Estimate")+theme_bw()+ggtitle(sprintf("Relative Risks -- Baseline=%s",baseline))
+    xlab(intervention)+ylab("Estimate")+theme_bw()+ggtitle(sprintf("Relative Risks\nBaseline=%s",baseline))+
+    theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
 }
 
 #' @import ggplot2
@@ -34,7 +36,7 @@ par_plot <- function(formatted_results){
   ggplot(pars,aes_string(x="strata_label", y="estimate", ymin="ci_lower", ymax="ci_upper"))+
     geom_point()+geom_errorbar()+
     xlab("")+ylab("Estimate")+theme_bw()+coord_flip()+
-    ggtitle(sprintf("Population Attributable Risks -- Baseline=%s",baseline))
+    ggtitle(sprintf("Population Attributable Risks\nBaseline=%s",baseline))
 }
 
 #' @import ggplot2
@@ -48,5 +50,5 @@ paf_plot <- function(formatted_results){
   ggplot(pafs,aes_string(x="strata_label", y="estimate", ymin="ci_lower", ymax="ci_upper"))+
     geom_point()+geom_errorbar()+
     xlab("")+ylab("Estimate")+theme_bw()+theme_bw()+coord_flip()+
-    ggtitle(sprintf("Population Attributable Fractions -- Baseline=%s",baseline))
+    ggtitle(sprintf("Population Attributable Fractions\nBaseline=%s",baseline))
 }

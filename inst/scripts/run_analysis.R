5b97be228ae7c5092ce7928b2c9ca7e2c4f91775
Jeremy Coyle <jeremyrcoyle@gmail.com>
2018-05-02 10:37:46 -0700

    enable bigger SL stack when adjusting

--- inst/scripts/run_analysis.R
+++ inst/scripts/run_analysis.R
@@ -23,7 +23,7 @@ configure_cluster("~/cluster_credentials.json")
 run_on_longbow(rmd_filename, sample_json, open_result = TRUE)
 
 ghap_test_json <- package_file("inst","sample_data","ghap_test.json")
-job_url <- run_on_longbow(rmd_filename, ghap_test_json, open_result = TRUE)
+job_id <- run_on_longbow(rmd_filename, ghap_test_json, open_result = TRUE)
 
 # publish your template for other users to use
 publish_template(rmd_filename, open_result = TRUE)
